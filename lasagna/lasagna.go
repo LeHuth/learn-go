971e51209202c4fd65efda1618364cfc58d80b37
leo@maczedong <leonard.huth@protonmail.com>
2026-02-25 23:59:26 +0100

    dsf

--- lasagna/lasagna.go
+++ lasagna/lasagna.go
@@ -1,6 +1,6 @@
 package lasagna
 
-// TODO: define the 'OvenTime' constant
+// OvenTime TODO: define the 'OvenTime' constant
 const OvenTime = 40
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
